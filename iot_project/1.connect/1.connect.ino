c15e936aebb080221e417148b52165da38b21c9c
kyuuinochi <52988090+kyuuinochi@users.noreply.github.com>
2024-08-27 15:28:09 +0800

    9600 to 115200
    9600 to 115200

--- iot_project/1.connect/1.connect.ino
+++ iot_project/1.connect/1.connect.ino
@@ -39,7 +39,7 @@ char pass[] = "pass";
 SoftwareSerial EspSerial(2, 3); // RX, TX
 
 // Your ESP8266 baud rate:
-#define ESP8266_BAUD 9600
+#define ESP8266_BAUD 115200
 
 ESP8266 wifi(&EspSerial);
 
